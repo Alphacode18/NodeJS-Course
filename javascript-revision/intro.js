e108c8f8b09563faeeae20da57df544043c6a8f6
Alphacode18 <skharban@purdue.edu>
2020-06-16 12:57:11 +0530

    Old variable and function syntax

---
+++ javascript-revision/intro.js
@@ -0,0 +1,9 @@
+var name = 'Shreyas';
+var age = 19;
+var hasHobbies = true;
+
+function userSummary (name, age) {
+    console.log(`I am ${name}, I am ${age} years old.`);
+}
+
+userSummary(name, age);
