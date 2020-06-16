ce751a8a14b7310658c17f1264b7208ba5cb781b
Alphacode18 <skharban@purdue.edu>
2020-06-16 13:19:04 +0530

    Objects Revised

--- javascript-revision/objects.js
+++ javascript-revision/objects.js
@@ -4,8 +4,17 @@ const person = {
     name : 'Shreyas',
     age : 19,
     greet: () => {
-        console.log(`Hi, I am ${person.name}`);
-    }
+        console.log("Hi, I am " + this.name); //Would generally print undefined because this refers
+                                              //To the global node runtime scope and not the object.
+    },
+};
+
+const person2 = {
+    name : 'Shreyas',
+    age : 19,
+    greet() {
+        console.log("Hi, I am " + this.name); //Prints correctly.
+    },
 };
 
 console.log(person);
