7c3de6e7f30c0e5d42e68d590d4b8f31bcd7cb7f
Alphacode18 <skharban@purdue.edu>
2020-06-16 13:01:11 +0530

    Let and Const Revised

---
+++ javascript-revision/letConst.js
@@ -0,0 +1,9 @@
+let name = 'Shreyas';
+let age = 19;
+const hasHobbies = true; //Use const as much more as possible.
+
+function userSummary(name, age) {
+    return `Nice to meet you ${name}. I am also ${age} years old`;
+}
+
+console.log(userSummary(name, age));
