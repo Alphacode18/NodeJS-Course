9ccea776df95f0c7915e414e2d833f71f543148a
Alphacode18 <skharban@purdue.edu>
2020-06-17 10:01:00 +0530

    Spread and Rest Operators

--- javascript-revision/restSpread.js
+++ javascript-revision/restSpread.js
@@ -1,3 +1,13 @@
 const hobbies = ['Sports', 'Cooking', 'Programming'];
-const copiedHobbies = [...hobbies];
+const copiedHobbies = [...hobbies]; //Spread is basically the ... followed by an object. It rips apart
+//all the properties of that object and assigns it to the data structure surrounding it.
 copiedHobbies.forEach(element => console.log(element));
+
+const person = {
+    name: 'Shreyas',
+    age: 19,
+    knowsNode: true,
+};
+
+const newPerson = {...person};
+console.log(newPerson);
