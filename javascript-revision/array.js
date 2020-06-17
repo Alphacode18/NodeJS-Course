4d5b1c0f5ffd7d4f370bf3e34925ee8f077e18e3
Alphacode18 <skharban@purdue.edu>
2020-06-17 09:45:32 +0530

    Arrays Revised

--- javascript-revision/array.js
+++ javascript-revision/array.js
@@ -1,3 +1,5 @@
 const hobbies = ['Sports', 'Cooking'];
 hobbies.forEach(element => console.log(element));
+const iLoveHobbies = hobbies.map(element => `I love ${element}`);
+iLoveHobbies.forEach(element => console.log(element));
 //Arrays are heterogenous and can use iterative functions which are basically higher order functions
