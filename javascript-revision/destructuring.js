7b291b9176bb62bab9e4498b74c4c1bf38bf97de
Alphacode18 <skharban@purdue.edu>
2020-06-17 10:11:18 +0530

    Destructuring revised

---
+++ javascript-revision/destructuring.js
@@ -0,0 +1,22 @@
+//Object Destructuring
+
+const person = {
+    firstName: 'Shreyas',
+    lastName: 'Kharbanda',
+    almaMater: 'Purdue University',
+    gradePointAverage: 4.0,
+    coursesTakes: ['CS180', 'CS182', 'CS240'],
+    isHonorsStudent: true,
+}
+
+const printName = personData => {
+    return personData.firstName;
+};
+
+//Drops all other properties and only extracts the required property.
+const printNameDestructuring = ({firstName}) => {
+    return firstName;
+};
+
+console.log(printName(person));
+console.log(printNameDestructuring(person));
