af5651467d49be1b311eeae3e40726805f39bd6e
Alphacode18 <skharban@purdue.edu>
2020-06-17 10:36:27 +0530

    Async Revised

--- javascript-revision/async.js
+++ javascript-revision/async.js
@@ -10,3 +10,22 @@ console.log('JavaScript is non-blocking');
 //In a sense here, JavaScript sees an asynchronous function, and it takes it as a callback. Then it utilizes it non-blocking
 //nature and moves on to the next synchronous function and then after execution is done, it comes back to the callback!
 
+//Over the course, timers and callbacks are difficult to maintain.
+
+const fetchData = callback => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done');
+        }, 1500);
+    });
+    return promise;
+}
+
+setTimeout(() => {
+    fetchData().then(text => {
+        return fetchData()
+    })
+    .then(text2 => {
+        console.log(tetx2);
+    })
+}, 2000);
