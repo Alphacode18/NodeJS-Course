63b2ba0c87a6b99e99662dee3b11441ee149b39c
Alphacode18 <skharban@purdue.edu>
2020-06-18 18:52:04 +0530

    Understood How To Create A Server

--- basics-node/server.js
+++ basics-node/server.js
@@ -1,5 +1,6 @@
 const http = require('http'); //Require used to import modules. It takes in a path or a core module as a arguement. Adds a .js at the end.
 const server = http.createServer((request, response) => {
     console.log(request);
+    //process.exit();
 });
 server.listen(3000);
