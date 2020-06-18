fd071b11ad0dbdf89dfa8133473c1394abcfb14a
Alphacode18 <skharban@purdue.edu>
2020-06-18 19:03:59 +0530

    Understood How To Write Responses

--- basics-node/server.js
+++ basics-node/server.js
@@ -1,6 +1,12 @@
 const http = require('http'); //Require used to import modules. It takes in a path or a core module as a arguement. Adds a .js at the end.
 const server = http.createServer((request, response) => {
-    console.log(request);
+    console.log(request.url, request.method, request.headers);
     //process.exit();
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>My First Page</title></head>');
+    response.write('<body><h1>Hello, World</h1></body>');
+    response.write('</html>');
+    response.end();
 });
 server.listen(3000);
