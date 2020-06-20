850bdd71ca911a1b4a6c6e8d70ee1fd67c605e9b
Alphacode18 <skharban@purdue.edu>
2020-06-20 17:03:09 +0530

    Error page created for server

--- workflow-development/server.js
+++ workflow-development/server.js
@@ -5,7 +5,20 @@ const server = http.createServer((request, response) => {
         response.setHeader('content-type', 'text/html');
         response.write('<html>');
         response.write('<head><title>Workflow Module</title></head>');
-        response.write('<body><h1>Workflow Development Example Server</h1></body>');
+        response.write('<body><h1>Workflow Development Example Server</h1>');
+        response.write('<h2>Test HTML element for Nodemon server package.</h2>');
+        response.write('<p>Lorem Ipsum</p><br>');
+        response.write('</body>');
+        response.write('</html>');
+        return response.end();
+    }
+    else {
+        response.statusCode = 404;
+        response.setHeader('content-type', 'text/html');
+        response.write('<html>');
+        response.write('<head><title>Workflow Module</title></head>');
+        response.write('<body><h1>The Page You Are Looking For Doesn\'t exist</h1>');
+        response.write('</body>');
         response.write('</html>');
         return response.end();
     }
