f593c0a4c8e7f8df7b6b77b06ad9c7abbe359f49
Alphacode18 <skharban@purdue.edu>
2020-06-20 11:41:27 +0530

    Assignment Completed

--- basics-node-assignment/server.js
+++ basics-node-assignment/server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const routes = require('./routes');
 const requestHandler = require('./routes');
 const server = http.createServer(requestHandler);
 server.listen(3000);
