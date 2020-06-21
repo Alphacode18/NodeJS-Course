11512937e0361487f5f94543d3f6724d8dc265e9
Alphacode18 <skharban@purdue.edu>
2020-06-21 17:02:09 +0530

    Express Router Understood

---
+++ express-shop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express(); 
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/', (req, res, next)=> {
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.listen(3000);
