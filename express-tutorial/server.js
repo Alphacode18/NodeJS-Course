96509a53a36f75891b7e71b79f4582b1bb362348
Alphacode18 <skharban@purdue.edu>
2020-06-21 11:29:41 +0530

    Middleware Explored

--- express-tutorial/server.js
+++ express-tutorial/server.js
@@ -1,3 +1,7 @@
+/*
+ * The extra logs you would see are generally because of the favicon requests by the browser.
+ */
+
 const http = require('http');
 
 const express = require('express'); //Import Express.js
@@ -11,6 +15,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another the middleware!');
+    res.send('<h1>Hello From Express</h1>'); //Express does the header setting for you.
 }); //Request and Response are pretty self explanatory. Next is another function being passed to the current callback.
 
 const server = http.createServer(app); //The app variable holds a valid event listener.
