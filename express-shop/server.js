2cc605c7b3e35f34def09424bb939b894db3d32f
Alphacode18 <skharban@purdue.edu>
2020-06-21 17:11:51 +0530

    Express Router Understood

--- express-shop/server.js
+++ express-shop/server.js
@@ -13,7 +13,10 @@ app.use('/', (req, res, next)=> {
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status('404').send('<h1>Page Not Found </h1>');
+})
 
 app.listen(3000);
