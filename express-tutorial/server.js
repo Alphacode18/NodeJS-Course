e21518a4c246ac1864cf111c520d2f0c254f125c
Alphacode18 <skharban@purdue.edu>
2020-06-21 16:42:25 +0530

    Body Parser Understood

--- express-tutorial/server.js
+++ express-tutorial/server.js
@@ -5,6 +5,7 @@
 //const http = require('http'); No HTTP Import required.
 
 const express = require('express'); //Import Express.js
+const bodyParser = require('body-parser');
 
 const app = express(); //Execute the function imported. This function does all the background set up for us regarding the object.
 
@@ -19,13 +20,23 @@ app.use((req, res, next) => {
 
 //app.use() is very versatile. It can be used as a lot of different definitions.
 
+//Parse Middleware
+
 app.use('/', (req, res, next)=> {
     next();
-})
+});
+
+app.use(bodyParser.urlencoded({extended:true})); //Parses body from forms and inside calls next to move onto the other middle ware.
 
 //In app.use(), the first argument is a a path. But it means that the path has to start with that, and not end. Thus, use top to bottom middleware.
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>The Add Product Page</h1>'); //Express does the header setting for you.
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>'); 
+    //Express does the header setting for you.
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body); //undefined because even though Express gives us the body, it does not parse it for us.
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
