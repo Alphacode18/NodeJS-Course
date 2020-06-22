64bd920f0bf001f8cefac217f8009c539b83dc5e
Alphacode18 <skharban@purdue.edu>
2020-06-22 18:02:51 +0530

    Serving HTML file Understood

--- express-shop/routes/shop.js
+++ express-shop/routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello From Express</h1>'); 
+    //res.send('<h1>Hello From Express</h1>'); 
+    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));//Path must be absolute but if you use '/', that'd be the root directory of the OS.
+    //Thus, we use the path core module. Path.join() takes in arguments and takes strings. Path.join() detects OS and constructs path accordingly.
 });
 
 module.exports = router;
