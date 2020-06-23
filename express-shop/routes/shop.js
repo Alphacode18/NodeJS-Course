adb4bb800600807a4eb18b529bd709bb14750f61
Alphacode18 <skharban@purdue.edu>
2020-06-23 07:41:05 +0530

    Routes Added

--- express-shop/routes/shop.js
+++ express-shop/routes/shop.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     //res.send('<h1>Hello From Express</h1>'); 
-    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));//Path must be absolute but if you use '/', that'd be the root directory of the OS.
+    res.sendFile(path.join(__dirname, '..','views', 'shop.html'));//Path must be absolute but if you use '/', that'd be the root directory of the OS.
     //Thus, we use the path core module. Path.join() takes in arguments and takes strings. Path.join() detects OS and constructs path accordingly.
 });
 
