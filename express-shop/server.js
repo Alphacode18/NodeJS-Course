9c62cb2107d28f1928a60ec1ba4e66c627b2b8f9
Alphacode18 <skharban@purdue.edu>
2020-06-25 17:36:11 +0530

    404 Page Rendered With HandleBars

--- express-shop/server.js
+++ express-shop/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const handlebars = require('express-handlebars@3.0');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.router); //first path adds a kind of prefix. Thus, it implicitly adds admin in the path inside adminRoutes.
 app.use(shopRoutes);
 
+//Way To Pass Data In Templating Engine Does Not Change
 app.use((req, res, next) => {
     res.status('404').render('404.pug', { pageTitle: 'Page Not Found' });
 })
