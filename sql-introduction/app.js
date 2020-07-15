eef0242f38134820be723776b273f838660b09f5
Alphacode18 <skharban@purdue.edu>
2020-07-15 17:28:22 +0530

    Sequelize Module

--- sql-introduction/app.js
+++ sql-introduction/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,9 +24,21 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync() //Method Syncs all the tables
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+    .sync() //Method Syncs all the tables
     .then(result => {
-        //console.log(result);
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({name: 'Shreyas Kharbanda', email: 'skkhary@gmail.com'});
+        }
+        return user;
+    })
+    .then(user => {
         app.listen(3000);
     })
     .catch(err => {
