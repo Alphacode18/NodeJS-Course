6091e4415f3f284c15da5aacc3c122f2a7e22d8c
Alphacode18 <skharban@purdue.edu>
2020-07-17 13:02:07 +0530

    Sequelize Module

--- sql-introduction/controllers/shop.js
+++ sql-introduction/controllers/shop.js
@@ -61,32 +61,16 @@ exports.getCart = (req, res, next) => {
     .catch(err => {
       console.log(err);
     });
-  // Cart.getCart(cart => {
-  //   Product.fetchAll(products => {
-  //     const cartProducts = [];
-  //     for (product of products) {
-  //       const cartProductData = cart.products.find(
-  //         prod => prod.id === product.id
-  //       );
-  //       if (cartProductData) {
-  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
-  //       }
-  //     }
-  //     res.render('shop/cart', {
-  //       path: '/cart',
-  //       pageTitle: 'Your Cart',
-  //       products: cartProducts
-  //     });
-  //   });
-  // });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  req.user
+    .getCart()
+    .then(cart => {
+      
+    })
+    .catch();
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
