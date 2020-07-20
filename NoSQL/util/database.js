87a07a6e58265dba74cadc011c371f19fbdfdf71
Alphacode18 <skharban@purdue.edu>
2020-07-20 09:36:12 +0530

    NoSQL Started

--- NoSQL/util/database.js
+++ NoSQL/util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient
+    .connect(
+      `mongodb+srv://Shreyas:${process.env.PASSWORD}@node-course.q3zz2.mongodb.net/shop?retryWrites=true&w=majority`
+  )
+    .then(client => {
+      console.log('Connected');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+}
+
+const getDB = () => {
+  if (_db) {
+    return _db
+  }
+  throw 'No Database Found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
