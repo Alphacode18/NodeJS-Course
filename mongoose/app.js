f7463d0a45c7ad0e909138305960d47b31d394f5
Alphacode18 <skharban@purdue.edu>
2020-07-29 18:31:13 +0530

    Mongoose Save

--- mongoose/app.js
+++ mongoose/app.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,12 +17,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findById('5baa2528563f16379fc8a610')
+  //   .then((user) => {
+  //     req.user = new User(user.name, user.email, user.cart, user._id);
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
+  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -34,7 +33,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://Shreyas:JfuB7VLhsMJIlNnv@node-course.q3zz2.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then((result) => {
     app.listen(3000);
